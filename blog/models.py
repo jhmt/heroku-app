e041d0b2e6f74b14b6e02e1e1bf94a55c9e64eb2
jhmt <jhashimot00@gmail.com>
2015-12-28 02:38:01 +1300

    Migration

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+﻿from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -15,17 +15,16 @@ class Post(models.Model):
 							unique_for_date='publish')
 	author = models.ForeignKey(User, 
 							   related_name='blog_posts')
-
 	body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, 
-                              choices=STATUS_CHOICES,
-                              default='draft')
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10, 
+							  choices=STATUS_CHOICES,
+							  default='draft')
 
-    class Meta:
-        ordering = ('-publish',)
+	class Meta:
+		ordering = ('-publish',)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
